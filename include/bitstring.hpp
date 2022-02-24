42a850ff4cd1ee8b74a3805e3e3ffb11f25a562d
Andreas Wallner <A.Wallner@innovative-solutions.at>
2022-02-24 15:46:05 +0100

    Add missing headers to bitstring.hpp

--- include/bitstring.hpp
+++ include/bitstring.hpp
@@ -1,6 +1,11 @@
 #ifndef header_bitstring_bitstring_hpp
 #define header_bitstring_bitstring_hpp
 
+// All-in-one include for bitstring
+
+#include "bitstring/exceptions.hpp"
+#include "bitstring/endian.hpp"
 #include "bitstring/bit_array.hpp"
+#include "bitstring/literals.hpp"
 
 #endif
